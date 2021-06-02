5d0f37f110a5b1aefed740132689517ecb439878
sergio-av <sergioavilachacon@gmail.com>
2021-06-02 15:40:31 +0200

    maquetacion register done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import Login from './components/Login/Login';
+import Register from './components/Register/register';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Register />
     </div>
   );
 }
---
+++ src/components/Register/register.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import './styleRegister.css';
+
+
+
+import logoRegister from '../../img/logoRegister.svg'
+
+const Register = () => {
+    return (
+        <div>
+            <div id="body">
+                <div id="contentRegister">
+                    <div id="titleRegister">
+                        <img src={logoRegister} alt="logo" />
+                        <h1>Bienvenido a la familia</h1>
+                    </div>
+                    <form action="">
+                        <div className="inputsRegister">
+                            <div>
+                                <label htmlFor="">Apodo:</label>
+                                <input type="text" id="nick" name="nick"/>
+                            </div>
+                            <hr className="hrRegister"/>
+                        </div>
+                        <div className="inputsRegister">
+                            <div>
+                                <label htmlFor="userName">Nombre:</label>
+                                <input type="text" id="userName" name="userName"/>
+                            </div>
+                            <hr className="hrRegister"/>
+                        </div>
+                        <div className="inputsRegister">
+                            <div>
+                                <label htmlFor="userPass">Contraseña:</label>
+                                <input type="password" id="userPass" name="userPass"/>
+                            </div>
+                            <hr className="hrRegister"/>
+                        </div>
+                        <div className="inputsRegister">
+                            <div>
+                                <label htmlFor="userPass">Gmail:</label>
+                                <input type="gmail" id="gmail" name="gmail"/>
+                            </div>
+                            <hr className="hrRegister"/>
+                        </div>
+                    </form>
+                    <button>Enviar solicitud</button>
+                    <p>¿Ya te unistes?</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Register;
